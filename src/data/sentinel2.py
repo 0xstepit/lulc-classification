70a92fb0e17dbf29dca8d3543a824c4396b48d3d
stepit <stefanofrancesco.pitton@gmail.com>
2026-05-29 12:40:17 +0200

    fear: add sentinel2 client

---
+++ src/data/sentinel2.py
@@ -0,0 +1,21 @@
+from pystac_client import Client
+
+from src.config import BBOX, Sentinel2Config
+
+
+class SentinelClient:
+    def __init__(self, cfg: Sentinel2Config) -> None:
+        self._client = Client.open(cfg.stac.url)
+        self._cfg = cfg
+
+    def search_scenes(self, bbox: BBOX) -> list:
+        year = self._cfg.aoi.year
+
+        search = self._client.search(
+            collections=[self._cfg.stac.collection],
+            bbox=list(bbox),
+            datetime=f"{year}-01-01/{year}-12-31",
+            query={"eo:cloud_cover": {"lt": self._cfg.aoi.max_cloud_coverage}},
+        )
+
+        return list(search.item_collection())
