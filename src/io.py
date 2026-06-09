de381c8a3cc9b0039da98f53d892b925d3bd21f7
stepit <stefanofrancesco.pitton@gmail.com>
2026-06-09 10:06:33 +0200

    chore: add comments to file

--- src/io.py
+++ src/io.py
@@ -2,8 +2,12 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent.parent
 
+# Folder containing all the data used in the project.
 DATA_DIR = ROOT / "data"
+# Folder containing raw scene generated or downloaded from Sentinel2 data.
 RAW_DIR = DATA_DIR / "raw"
+# Folder containing automatic analysis results.
 ANALYSIS_DIR = DATA_DIR / "analysis"
 
+# Folder containing all the configuration files for the scripts.
 CONFIG_DIR = ROOT / "config"
