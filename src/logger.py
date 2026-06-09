0c4b4562125ba8b1433ccdacffdc288a5bae0fce
stepit <stefanofrancesco.pitton@gmail.com>
2026-06-09 14:40:02 +0200

    chore: minor update logger and uv

--- src/logger.py
+++ src/logger.py
@@ -4,8 +4,18 @@ from rich.logging import RichHandler
 
 
 def setup_logging(level: int = logging.INFO) -> None:
+    """Common logging configuration handler. This function has to be called
+    before every logger instantiation in all the files that require logs.
+
+    Parameters
+    ----------
+    level : int
+        Level of the logger.
+
+
+    """
     logging.basicConfig(
-        level=logging.INFO,
+        level=level,
         format="%(message)s",
         handlers=[RichHandler(rich_tracebacks=True)],
     )
