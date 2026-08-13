ccd0af7174b41f0162e1a41850c47f80ee880bbe
stepit <stefanofrancesco.pitton@gmail.com>
2026-08-13 10:29:24 +0200

    docs: add docstring to viz library

--- src/viz/colors.py
+++ src/viz/colors.py
@@ -1,2 +1,13 @@
-def normalize_rgb(rgb):
+def normalize_rgb(rgb: list[int]):
+    """Normalize the RGB values into the range [0, 1] by dividing by 255.
+
+    Parameters
+    ----------
+    rgb :
+        A list containing the three RGB values
+
+    Returns
+    -------
+        The RGB values rescaled into [0, 1]
+    """
     return tuple([x / 255 for x in rgb])
--- src/viz/images.py
+++ src/viz/images.py
@@ -9,6 +9,15 @@ IMGS_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def store_figure(title: str, transparent: bool = True):
+    """Store the the active figure using the provided title as file name.
+
+    Parameters
+    ----------
+    title : str
+        The title of the figure that is converter into the file name without spaces.
+    transparent : bool
+        Whether the backgeound of the figure must be transparent or not
+    """
     _title = title.lower().replace(" ", "_").replace("(", "").replace(")", "")
     plt.savefig(
         (IMGS_DIR / _title).with_suffix(".png"),
@@ -20,6 +29,13 @@ def store_figure(title: str, transparent: bool = True):
 
 
 def set_matplotlib_global_config(cfg: VizConfig) -> None:
+    """Set the matplotlib parameters in the provided configuration as global params.
+
+    Parameters
+    ----------
+    cfg : VizConfig
+        The config instance containing global params.
+    """
     prop_cycle_colors = cfg.colors.prop_cycle
     colors = [prop_cycle_colors[idx] for idx in range(len(prop_cycle_colors.keys()))]
     mpl.rcParams["axes.prop_cycle"] = cycler(color=colors)
