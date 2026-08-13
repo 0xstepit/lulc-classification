906afc58c6a3d654463208413c5b1098f20cff22
stepit <stefanofrancesco.pitton@gmail.com>
2026-08-13 10:11:57 +0200

    improvement: hardened logger

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import logging
 from rich.logging import RichHandler
 
 
-def setup_logging(level: int = logging.INFO) -> None:
+def setup_logging(level: int = logging.INFO, force: bool = False) -> None:
     """Common logging configuration handler. This function has to be called
     before every logger instantiation in all the files that require logs.
 
@@ -16,5 +16,10 @@ def setup_logging(level: int = logging.INFO) -> None:
     logging.basicConfig(
         level=level,
         format="%(message)s",
+        datefmt="[%X]",
         handlers=[RichHandler(rich_tracebacks=True)],
+        force=force,
     )
+    logging.captureWarnings(
+        True
+    )  # used to capture rasterio warnings in the same stream
